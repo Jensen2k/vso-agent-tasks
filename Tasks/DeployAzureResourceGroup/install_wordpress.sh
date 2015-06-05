60a8757a1d191c32c69c4f97e8d9cdc0d991569f
Nikhil Ranjan Kumar <nikhilku@microsoft.com>
2015-06-05 19:37:00 +0530

    fix

--- Tasks/DeployAzureResourceGroup/install_wordpress.sh
+++ Tasks/DeployAzureResourceGroup/install_wordpress.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+apt-get -y update
+
+logger "Installing WordPress"
+
+# Set up a silent install of MySQL
+dbpass=$1
+
+export DEBIAN_FRONTEND=noninteractive
+echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
+echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
+
+# Install the LAMP stack and WordPress
+apt-get -y install apache2 mysql-server php5 php5-mysql wordpress
+
+# Setup WordPress
+gzip -d /usr/share/doc/wordpress/examples/setup-mysql.gz
+bash /usr/share/doc/wordpress/examples/setup-mysql -n wordpress localhost
+
+ln -s /usr/share/wordpress /var/www/html/wordpress
+mv /etc/wordpress/config-localhost.php /etc/wordpress/config-default.php
+
+# Restart Apache
+apachectl restart
+
+logger "Done installing WordPress; open /wordpress to configure"
+
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/7u75-b13/jdk-7u75-linux-x64.tar.gz"
 tar -xvf jdk-7*
 mkdir /usr/lib/jvm
@@ -28,30 +54,4 @@ echo "server.2=10.0.0.5:2888:3888" >> zookeeper-3.4.6/conf/zoo.cfg
 echo "server.3=10.0.0.6:2888:3888" >> zookeeper-3.4.6/conf/zoo.cfg
 
 mkdir -p /var/lib/zookeeper
-
-apt-get -y update
-
-logger "Installing WordPress"
-
-# Set up a silent install of MySQL
-dbpass=$1
-
-export DEBIAN_FRONTEND=noninteractive
-echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
-echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
-
-# Install the LAMP stack and WordPress
-apt-get -y install apache2 mysql-server php5 php5-mysql wordpress
-
-# Setup WordPress
-gzip -d /usr/share/doc/wordpress/examples/setup-mysql.gz
-bash /usr/share/doc/wordpress/examples/setup-mysql -n wordpress localhost
-
-ln -s /usr/share/wordpress /var/www/html/wordpress
-mv /etc/wordpress/config-localhost.php /etc/wordpress/config-default.php
-
-# Restart Apache
-apachectl restart
-
-logger "Done installing WordPress; open /wordpress to configure"
 
